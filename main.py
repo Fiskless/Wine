b14424482a892567d8ab36ea8bd31364e29bc43e
pavel <paul601@mail.ru>
2021-08-09 22:59:53 +0500

    Add profitable promotion mark

--- main.py
+++ main.py
@@ -13,34 +13,21 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Sheet1',
+excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Sheet1',
                                   usecols=['Category', 'Name', 'Sort',
-                                           'Price', 'Picture'],
+                                           'Price', 'Picture', 'Discount'],
                                   na_values='some_dummy_na_value',
                                   keep_default_na=False)
 wine_data = excel_data_df.to_dict(orient='records')
 
-# categories_list = []
-# for wine in wine_data:
-#     categories_list.append(wine['Категория'])
-
-# wine_dict = {}
-# for category in set(categories_list):
-#     wine_dict[category] = [wine for wine in wine_data if category == wine['Категория']]
-
-
 drinks_dict = collections.defaultdict(list)
 for wine in wine_data:
     drinks_dict[wine['Category']].append(wine)
-pprint(drinks_dict)
-
-
 
 rendered_page = template.render(
     winery_age=datetime.datetime.now().year-1920,
     drinks_data=drinks_dict,
 )
-
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
