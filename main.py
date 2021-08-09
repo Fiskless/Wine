a413ba07a0dc2bc8d70ce343f87f4a33f5337427
pavel <paul601@mail.ru>
2021-08-09 20:24:53 +0500

    Get wine data from excel file using defaultdict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
 import pandas
+from pprint import pprint
+import collections
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -11,14 +13,33 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1',
-                                  usecols=['Название', 'Сорт',
-                                           'Цена', 'Картинка'])
+excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1',
+                                  usecols=['Категория', 'Название', 'Сорт',
+                                           'Цена', 'Картинка'],
+                                  na_values='some_dummy_na_value',
+                                  keep_default_na=False)
 wine_data = excel_data_df.to_dict(orient='records')
+
+# categories_list = []
+# for wine in wine_data:
+#     categories_list.append(wine['Категория'])
+
+# wine_dict = {}
+# for category in set(categories_list):
+#     wine_dict[category] = [wine for wine in wine_data if category == wine['Категория']]
+
+
+wine_dict = collections.defaultdict(list)
+for wine in wine_data:
+    wine_dict[wine['Категория']].append(wine)
+pprint(wine_dict)
+
+
+
 
 rendered_page = template.render(
     winery_age=datetime.datetime.now().year-1920,
-    wine_data=wine_data,
+    # wine_data=wine_data,
 )
 
 
@@ -27,3 +48,4 @@ with open('index.html', 'w', encoding="utf8") as file:
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
+
