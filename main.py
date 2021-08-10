b9f4e46bc01288470938ac6448fa373f65525b95
pavel <paul601@mail.ru>
2021-08-10 20:52:14 +0500

    Rename variables

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Sheet1',
                                            'Цена', 'Картинка', 'Акция'],
                                   na_values='some_dummy_na_value',
                                   keep_default_na=False)
-wine_data = excel_data_df.sort_values('Категория').to_dict(orient='records')
+drinks_unsorted = excel_data_df.sort_values('Категория').to_dict(orient='records')
 
-drinks_dict = collections.defaultdict(list)
-for wine in wine_data:
-    drinks_dict[wine['Категория']].append(wine)
+drinks_sorted_by_category = collections.defaultdict(list)
+for drink in drinks_unsorted:
+    drinks_sorted_by_category[drink['Категория']].append(drink)
 
 rendered_page = template.render(
     winery_age=datetime.datetime.now().year-1920,
-    drinks_data=drinks_dict,
+    drinks_data=drinks_sorted_by_category,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
