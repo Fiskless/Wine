ee133e67ba2969b80062fe49418770904f776a2a
pavel <paul601@mail.ru>
2021-08-10 20:45:58 +0500

    Change columns language in table and script

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import datetime
-import pandas
 import collections
+import datetime
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(
@@ -13,15 +13,15 @@ env = Environment(
 template = env.get_template('template.html')
 
 excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Sheet1',
-                                  usecols=['Category', 'Name', 'Sort',
-                                           'Price', 'Picture', 'Discount'],
+                                  usecols=['Категория', 'Имя', 'Сорт',
+                                           'Цена', 'Картинка', 'Акция'],
                                   na_values='some_dummy_na_value',
                                   keep_default_na=False)
-wine_data = excel_data_df.sort_values('Category').to_dict(orient='records')
+wine_data = excel_data_df.sort_values('Категория').to_dict(orient='records')
 
 drinks_dict = collections.defaultdict(list)
 for wine in wine_data:
-    drinks_dict[wine['Category']].append(wine)
+    drinks_dict[wine['Категория']].append(wine)
 
 rendered_page = template.render(
     winery_age=datetime.datetime.now().year-1920,
