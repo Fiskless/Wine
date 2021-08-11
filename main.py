4f79ccef910ee8e5d42ce32207b99d0300a4ac9d
pavel <paul601@mail.ru>
2021-08-12 00:47:16 +0500

    Add parser and enviromental variables

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import argparse
 import collections
 import datetime
+import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -22,14 +25,29 @@ def get_drinks_sorted_by_category_from_excel(file_name):
     return drinks_sorted_by_category
 
 
+def create_parser(path_to_file_with_drinks):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_file_with_drinks',
+                        help='Введите путь к вашему Excel файлу с напитками',
+                        nargs='?',
+                        default=path_to_file_with_drinks)
+    return parser
+
+
 def main():
+
+    load_dotenv()
+
+    parser = create_parser(os.getenv("PATH_TO_EXCEL_FILE"))
+    args = parser.parse_args()
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
 
-    drinks_sorted_by_category = get_drinks_sorted_by_category_from_excel('wine.xlsx')
+    drinks_sorted_by_category = get_drinks_sorted_by_category_from_excel(args.path_to_file_with_drinks)
 
     rendered_page = template.render(
         winery_age=datetime.datetime.now().year - 1920,
